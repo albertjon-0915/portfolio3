2a6c9e808877d0602fa74cc40f3fb2d8ef1e8326
albert.jon.inciong.0915 <albert.jon.inciong.0915@gmail.com>
2024-08-22 11:08:17 +0800

    homepage

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "../styling/home/about.scss";
 import coding from "../assets/coding.png";
+import { FaFacebookF, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 function About() {
      return (
           <div className="about-container">
                <div className="about-wrapper">
                     <img src={coding} alt="img" />
+
                     <div className="about-title">
                          <div className="title-focal">Want to know more about me &mdash; here's some brief info</div>
-                         <div className="socials"></div>
                     </div>
+
                     <div className="content">
-                         <h2>About</h2>
+                         <h3>About</h3>
                          <p className="about-text-content">
                               Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, cumque maxime. Dicta
                               dolore, fugiat labore eveniet perspiciatis nam deleniti animi placeat modi omnis dolores
@@ -20,6 +22,23 @@ function About() {
                               iusto tempore ratione fugit, cupiditate aliquam quidem odit mollitia similique eius
                               impedit autem maiores?
                          </p>
+                    </div>
+
+                    <div className="socials">
+                         <ul>
+                              <li className="item1">
+                                   <FaFacebookF />
+                                   <span>facebook</span>
+                              </li>
+                              <li className="item2">
+                                   <FaLinkedinIn />
+                                   <span>linkedIn</span>
+                              </li>
+                              <li className="item3">
+                                   <FaGithub />
+                                   <span>github</span>
+                              </li>
+                         </ul>
                     </div>
                </div>
           </div>
